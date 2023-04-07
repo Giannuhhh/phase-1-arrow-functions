cfcf65e9e4b4c4055b1c9e9c7141b93833202988
Gianna Mastro <giannamastro02@gmail.com>
2023-04-07 17:15:11 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,18 @@
+function divide() {
+    let a = 2000;
+    let b = 100;
+    return a / b;
+}
+divide(a,b);
+
+const square = x => {
+    return x * x;
+}
+square(2);
+
+
+const add = (a, b) => a + b;
+let a = 3;
+let b = 4;
+
+add(a + b); 
